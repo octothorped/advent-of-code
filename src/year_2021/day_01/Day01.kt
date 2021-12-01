a4de80db0767811928187076a4abe2c2c0b238d5
Eric Takemoto <24865872+octothorped@users.noreply.github.com>
2021-11-30 23:00:25 -0800

    day 1 - part 2 solution with enhancements

--- src/year_2021/day_01/Day01.kt
+++ src/year_2021/day_01/Day01.kt
@@ -6,23 +6,41 @@ fun main() {
     // Goal: count increasing values
     fun part1(input: List<String>): Int {
         var increasingCount = 0
-        input.forEachIndexed { index, s ->
-            if (index > 0) {
-                s.toInt()
-                    .takeIf { it > input[index - 1].toInt() }
-                    ?.also { increasingCount++ }
+        input
+            .map { it.toInt() }
+            .also { inputAsInt ->
+                inputAsInt.forEachIndexed { index, value ->
+                    index.takeIf { it > 0 && value > inputAsInt[index - 1] }
+                        ?.also { increasingCount++ }
+                }
             }
-        }
         return increasingCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increasingCount = 0
+        input
+            .map { it.toInt() }
+            .let { inputAsInt ->
+                inputAsInt.forEachIndexed { index, value ->
+                    index.takeIf { index > 2 }
+                        ?.let {
+                            (value + inputAsInt[index - 1] + inputAsInt[index - 2]) to
+                                    (inputAsInt[index - 1] + inputAsInt[index - 2] + inputAsInt[index - 3])
+                        }
+                        ?.takeIf { (currentValue, previousValue) ->
+                            currentValue > previousValue
+                        }
+                        ?.also { increasingCount++ }
+                }
+            }
+        return increasingCount
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("year_2021/day_01/Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("year_2021/day_01/Day01")
     println(part1(input))
